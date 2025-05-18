36e841315897cfeb19573c942ac2b575d9e7d0ce
badr <badr.grandpierre@lycee-jeanrostand.fr>
2025-05-18 15:57:20 +0200

    Delete preload.js

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  loginUser: (credentials) => ipcRenderer.invoke('login-user', credentials),
-});
