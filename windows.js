3ad53656c268b45f17c27c3bed0e9e4755a59212
Chaima120 <chaima.bejaoui@lycee-jeanrostand.fr>
2025-05-22 12:01:17 +0200

    Delete windows.js

--- windows.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('minimize').addEventListener('click', () => {
-  window.electronAPI.minimize();
-});
-
-document.getElementById('close').addEventListener('click', () => {
-  window.electronAPI.close();
-});
